c5cf9fae90eafe1df13f7e99d06259b81c181adf
Morsalin <mdmorsalinislam027@gmail.com>
2023-05-02 13:22:15 +0600

    chef data add

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
-const port= process.env.PORT || 5000
+const port = process.env.PORT || 5000
+
+const allChef = require("./jsonFile/chefData.json")
 
 
 app.get('/', (req, res) => {
-    res.send('server side go')
+    res.send('Wellcome to my server side')
+})
+
+app.get('/allChef', (req, res) => {
+    res.send(allChef)
 })
 
 app.listen(port, () => {
